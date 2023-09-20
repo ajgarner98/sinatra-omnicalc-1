f41e72da887baab84617ae5c039d69fe6886ad25
Andre Garner <111984130+ajgarner98@users.noreply.github.com>
2023-09-20 00:35:52 +0000

    Finished making Square Roots

--- app.rb
+++ app.rb
@@ -30,3 +30,9 @@ get ("/square/results") do
   @the_result = @the_num ** 2
   erb(:square_results)
 end
+
+get ("/square_root/results") do
+  @the_num = params.fetch("users_number").to_f
+  @the_result = @the_num ** 0.5
+  erb(:square_root_results)
+end
